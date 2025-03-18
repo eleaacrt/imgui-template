407404d92abd0a60937e48ab63a156ea82d07c6c
Eléa Crunchant <eleaa.crt@gmail.com>
2025-03-18 11:58:27 +0100

    new imgui template

--- src/App.cpp
+++ src/App.cpp
@@ -7,26 +7,24 @@ void App::init() const
 
 void App::update()
 {
-    // update the app
     quick_imgui::loop(
-        "Chess",
-        /* init: */ [&]() {
-            load_font("../../src/assets/font/CHEQ_TT.TTF", 64.0f);
-            ImGui::GetStyle().ItemSpacing = ImVec2(0.0f, 0.0f); },
-        /* loop: */
-        [&]() {
-            ImGui::Begin("CHESS GAME");
-
-            if (ImGui::IsMouseClicked(ImGuiMouseButton_Right))
-            {
-                std::cout << "right click" << '\n';
-                board.reset_turn();
-                // return;
-            }
-
-            board.draw_board();
-
-            ImGui::End();
+        "chess game",
+        {
+            .init                     = [&]() {            load_font("../../src/assets/font/CHEQ_TT.TTF", 64.0f);
+                ImGui::GetStyle().ItemSpacing = ImVec2(0.0f, 0.0f); },
+            .loop                     = [&]() {
+                if (ImGui::IsMouseClicked(ImGuiMouseButton_Right))
+                {
+                    std::cout << "right click" << '\n';
+                    board.reset_turn();
+                    // return;
+                }
+                board.draw_board(); },
+            .key_callback             = [](int key, int scancode, int action, int mods) { std::cout << "Key: " << key << " Scancode: " << scancode << " Action: " << action << " Mods: " << mods << '\n'; },
+            .mouse_button_callback    = [](int button, int action, int mods) { std::cout << "Button: " << button << " Action: " << action << " Mods: " << mods << '\n'; },
+            .cursor_position_callback = [](double xpos, double ypos) { std::cout << "Position: " << xpos << ' ' << ypos << '\n'; },
+            .scroll_callback          = [](double xoffset, double yoffset) { std::cout << "Scroll: " << xoffset << ' ' << yoffset << '\n'; },
+            .window_size_callback     = [](int width, int height) { std::cout << "Resized: " << width << ' ' << height << '\n'; },
         }
     );
 }
