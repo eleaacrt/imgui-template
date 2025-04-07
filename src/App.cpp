70c20093bd7dccc9966cae6cebab3e428614f346
Eléa Crunchant <eleaa.crt@gmail.com>
2025-04-07 14:46:45 +0200

    setup glmax

--- src/App.cpp
+++ src/App.cpp
@@ -1,9 +1,13 @@
 #include "headers/App.hpp"
+#include <Camera.hpp>
+#include <Shader.hpp>
 #include <glm/glm.hpp>
 #include <optional>
 #include <quick_imgui/quick_imgui.hpp>
+#include "Model3D.hpp"
 #include "glimac/FilePath.hpp"
 #include "glimac/Program.hpp"
+#include "glm/ext/matrix_clip_space.hpp"
 
 /* ----------------------
 
@@ -20,16 +24,50 @@ void App::update(int argc, char** argv)
     glimac::FilePath               applicationPath(argv[0]);
     std::optional<glimac::Program> program;
 
+    int           window_width  = 1280;
+    int           window_height = 720;
+    glmax::Shader shader;
+    glmax::Camera camera{true};
+
+    Model3D model;
+
     quick_imgui::loop(
         "",
         {
             .init         = [&]() {
                 load_font(applicationPath.dirPath() + "assets/font/CHEQ_TT.TTF", 64.0f);
-                program =
-                    loadProgram(applicationPath.dirPath() + "shaders/triangle.vs.glsl", applicationPath.dirPath() + "shaders/triangle.fs.glsl");
-                program->use();
-                board3D.initBoard3D(); },
+                // program =
+                //     loadProgram(applicationPath.dirPath() + "shaders/triangle.vs.glsl", applicationPath.dirPath() + "shaders/triangle.fs.glsl");
+                // program->use();
+                // board3D.initBoard3D(); 
+                shader.load_shader("model.vs.glsl", "model.fs.glsl");
+                // model.load_mesh("creeper/creeper.obj", "creeper");
+                model.load_mesh("board/board.obj", "board");
+                model.setup_buffers(); },
             .loop         = [&]() {
+                //
+                glClearColor(0.847f, 0.82f, 0.929f, 1.f);
+                glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+                glEnable(GL_DEPTH_TEST);
+                //
+                glm::mat4 projection = glm::perspective(glm::radians(45.0f), static_cast<float>(window_width) / static_cast<float>(window_height), 0.1f, 100.0f);
+
+                shader.use();
+                
+                //MVP
+                shader.set_uniform_matrix_4fv("model", glm::mat4(1.0f));
+                shader.set_uniform_matrix_4fv("view", camera.get_view_matrix());
+                shader.set_uniform_matrix_4fv("projection", projection);
+
+                //LIGHT SETTINGS
+                shader.set_uniform_3fv("lightPos", glm::vec3(5.0f, 5.0f, 5.0f));
+                shader.set_uniform_3fv("lightColor", glm::vec3(1.0f, 1.0f, 1.0f));
+                
+                //CAMERA SETTINGS
+                shader.set_uniform_3fv("viewPos", camera.get_position());
+                
+                //MODEL RENDER
+                model.render(shader);
                 if (ImGui::IsMouseClicked(ImGuiMouseButton_Right))
                 {
                     std::cout << "right click" << '\n';
@@ -37,7 +75,8 @@ void App::update(int argc, char** argv)
                     // return;
                 }
                 board.draw_board(); 
-                board3D.drawBoard3D(); },
+                // board3D.drawBoard3D(); 
+            },
             .key_callback = [](int key, int scancode, int action, int mods) {
                 // std::cout << "Key: " << key << " Scancode: " << scancode << " Action: " << action << " Mods: " << mods << '\n';
             },
